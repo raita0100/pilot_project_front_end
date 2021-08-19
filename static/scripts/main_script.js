04328b913f19afc9dfe813e8cef397c8429bd9e1
= <raita0100@gmail.com>
2021-08-19 18:09:35 +0530

    Added pagination and comments to code

--- static/scripts/main_script.js
+++ static/scripts/main_script.js
@@ -1,20 +1,33 @@
+/* Variables to store the url*/
 let url_call = "";
 let host_url = "http://localhost:8080/";
 let offset = 0;
+
+/*On ready apply logic*/
 $('document').ready(function() {
 
   /*
-    get Data
+    get Data without any sorting
   */
 
+  /*Rest call for spring host*/
   url_call = "getData/";
 
   fetch_data(host_url+url_call+offset);
 
 });
 
-/* Function for displaying the data*/
+
 function display_data(data){
+  /*
+    Function for displaying the data fetched in table
+    Parameteres:
+    -----------
+      data: (Json) data from server
+    Returns:
+    -----------
+      no return data
+  */
 
   items = [];
   for (var i = 0; i < data.length; i+=1){
@@ -32,15 +45,42 @@ function display_data(data){
   }).appendTo('#main_table');
 }
 
-/*Function to get the data by sorting*/
+
 function get_sorted_data(col, ord){
+  /*
+    Function for fetch data based on sorting parameter given
+    Parameteres:
+    -----------
+      col: (string) which parameter need to be sorted
+      ord: (string) Order of sorting ascending or descending
+
+    Returns:
+    -----------
+      no return data
+
+  */
+
   //console.log("\nSort CALL : "+col+" "+ord);
   url_call = "sort/"+col+"/"+ord+"/";
   fetch_data(host_url+url_call+offset);
 }
 
-/*function to fetch the data from server using ajax call*/
 function fetch_data(url){
+  /*
+    Function for Fetching the data from server
+    --> using ajax request
+    --> display the data in table
+    --> set pagination
+
+    Parameteres:
+    -----------
+      url: (string) url to ping
+
+    Returns:
+    -----------
+      no return data
+  */
+
   //console.log(url);
   $.ajax({
     url: url,
@@ -48,6 +88,7 @@ function fetch_data(url){
     dataType:'json',
     success: function(resp){
       display_data(resp);
+      init_paging();
     },
     error: function(xhr, option, err){
       console.log(err);
